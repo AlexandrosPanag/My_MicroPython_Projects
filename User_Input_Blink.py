d90179c5ad9512a79a26e71271b1cf88b00be254
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2022-07-01 13:05:00 +0300

    Add files via upload

--- User_Input_Blink.py
+++ User_Input_Blink.py
@@ -1,25 +1,28 @@
-# User Input Blink
-#
-# Created by Alexandros Panagiotakopoulos
-#
-
 from machine import Pin
 import time
 
 # Pin D9 (ON/SLEEP/DIO9)
 LED_PIN_ID = "D9"
+cnt=0
+i=0
 
-
+# Set up the LED pin object to manage the LED status. Configure the pin
+# as output and set its initial value to off (0).
 led_pin = Pin(LED_PIN_ID, Pin.OUT, value=0)
 
-# Force the user to enter in one of the two states
+# Start blinking the LED by toggling its value every second.
 while True:
-    x = input("Give one of the following numbers 1 to turn on the led or 0 to turn off the led:")
-    if x == "1":
+    print("\nGive the number of times for the device to blink:\n")
+    x = int(input())
+    print("\nThe device will blink", x,"times")
+    for i in range(0,x):
         print("- LED OFF")
         led_pin.value(0)
-        time.sleep(10)
-    elif x == "0":
+        time.sleep(1)
+
         print("- LED ON")
         led_pin.value(1)
-        time.sleep(10)
+        time.sleep(1)
+        cnt+=1
+        print("\nThe LED has blinked", cnt, " times\n")
+
